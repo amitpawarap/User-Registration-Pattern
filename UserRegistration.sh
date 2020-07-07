5b1ae45dc57f6f885a6364a369950c05151615f3
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-07 13:20:01 +0530

    Validate first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,14 @@
 #User Registration using Pattern Matching
 #!/bin/bash -x
+#validate first name
+echo "Enter The FirstName:"
+read FNAME;
+PAT="^[A-Z]{1}[a-z]{2,}$";
+if [[ $FNAME =~ $PAT ]]
+then
+echo "FirstName Is Valid";
+else
+echo "FirstName Is Not Valid";
+fi
+
+
